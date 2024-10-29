f2a8fbb85e01f7cfcb9bf8644e859fbdec606b6c
AdarshVishwakarma <38201144+proaddy@users.noreply.github.com>
2024-10-29 13:20:55 +0530

    look complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./public/pages/**/*.html"
+  ],
   theme: {
     extend: {
       fontFamily: {
